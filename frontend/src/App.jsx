e1875e83900cd7f85a0c102122e1c0cbcb054c62
jeewa-hash <jkumarasekara@gmail.com>
2024-08-27 15:02:46 +0530

    add comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LeaveRequestForm from './leaveReqForm/LeaveForm';
-import SalaryMang from './SalaryMangment/SalaryMang';
-import AttendanceTrackingForm from './AttendanceT/Attencance';
-import HRManagementInterface from './HRManagementInterface/HRInterface';
-import SalaryRet from './SalaryDetailsRet/SalaryRet';
-import HRManagementReport from './HRreport/HRreport';
-import ARet from './ATrackingRet/ARet'
-import LeaveRet from './LeaveRetrive/LeaveRet'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import LeaveRequestForm from './leaveReqForm/LeaveForm';
+//import SalaryMang from './SalaryMangment/SalaryMang';
+//import AttendanceTrackingForm from './AttendanceT/Attencance';
+//import HRManagementInterface from './HRManagementInterface/HRInterface';
+//import SalaryRet from './SalaryDetailsRet/SalaryRet';
+//import HRManagementReport from './HRreport/HRreport';
+//import ARet from './ATrackingRet/ARet'
+//import LeaveRet from './LeaveRetrive/LeaveRet'
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 // Importing components
-import OrderManagementSystem from './OrderManagement/order';
-import ProductionManagementSystem from './ProductionManagement/productionPlaning';
-import ResourcePlanningSystem from './ProductionManagement/ResourcesPlaning';
-import OrderAndProduction from './OrderAndProductionUi/OrderAndProduction';
-import OrderAndProductionReport from './OrderandProductionReport/OrderandProductionReport';
+//import OrderManagementSystem from './OrderManagement/order';
+//import ProductionManagementSystem from './ProductionManagement/productionPlaning';
+//import ResourcePlanningSystem from './ProductionManagement/ResourcesPlaning';
+//import OrderAndProduction from './OrderAndProductionUi/OrderAndProduction';
+//import OrderAndProductionReport from './OrderandProductionReport/OrderandProductionReport';
 
 const App = () => {
   return (
