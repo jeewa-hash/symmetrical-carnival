94a9b3eee4cc59f21f2086f6255fb845b1482bf2
jeewa-hash <jkumarasekara@gmail.com>
2024-08-27 15:14:31 +0530

    finance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <Router>
-      <Home />
-       
-       <Financereport/>
-        <Salaryret />
-        <About/>
-      <div>
-        
-      <Routes>
-          <Route exact path="/" element={<Financeuserinterface />} />
-          <Route path="/salarycalculateinterface" element={<Salarycalculateinterface />} />
-          <Route path="/orderbillinterface" element={<Orderbillinterface />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
   );
 };
 
